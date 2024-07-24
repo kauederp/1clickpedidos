f478f0dd1ddaf4e29ec58c4ed7ba9236a1ecd39d
kauederp <kauederp@gmail.com>
2024-07-24 17:09:46 -0300

    change opcoes css to 2 columns

--- painel/produtos/editar/index.php
+++ painel/produtos/editar/index.php
@@ -565,7 +565,7 @@ if ($formdata) {
                                                                   <span class="d-none material-symbols-outlined">
                                                                     visibility_off
                                                                   </span>
-                                                                  <?php echo htmljson($variacao[$x]['item'][$y]['valor'])?>
+                                                                  <?php echo htmljson($variacao[$x]['item'][$y]['esconder']);?>
                                                                   <input class="esconder-input" type="hidden" name='variacao[<?php echo $x; ?>][item][<?php echo $y; ?>][esconder]'>
                                                                 </div>
                                                               </div>
