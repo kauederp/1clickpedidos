19218512b09fa9c7605e2b88f0af9f00962a9606
kauederp <kauederp@gmail.com>
2024-07-30 20:55:02 -0300

    adição de htmljson no editar/index

--- painel/produtos/editar/index.php
+++ painel/produtos/editar/index.php
@@ -492,7 +492,7 @@ if ($formdata) {
 
               <?php
               $variacao = json_decode($data['variacao'], TRUE);
-              echo $variacao;
+              echo htmljson($variacao[0]['nome']);
               for ($x = 0; $x < count($variacao); $x++) {
               ?>
 
